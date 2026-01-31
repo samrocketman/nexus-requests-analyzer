96e5b027717d34098b3ec6602e31d6f354c98ce3
Sam Gleske <sam.mxracer@gmail.com>
2026-01-31 10:08:11 -0500

    Separate same client calling within same second to separate client

--- replay_requests/requests-to-tsv.sh
+++ replay_requests/requests-to-tsv.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo $'timestamp_offset\tclient_id\thttp_method\tpath\tuseragent'
-yq -r -o=tsv '.requests[0].unix_time as $mod | .requests[] | [.unix_time % $mod, .host + "." + .useragent_id, .http_method, .path, .useragent]'
+yq -r -o=tsv '
+  .requests[0].unix_time as $mod |
+  [.requests[] | . + {"_offset": (.unix_time % $mod)}] |
+  group_by(._offset) |
+  map(to_entries | map(.value + {"_seq": .key})) |
+  flatten |
+  sort_by(.unix_time) |
+  .[] |
+  [._offset, (.host + "." + .useragent_id + "." + (._offset | tostring) + "." + (._seq | tostring)), .http_method, .path, .useragent]
+'
+#yq -r -o=tsv '.requests[0].unix_time as $mod | .requests[] | [.unix_time % $mod, .host + "." + .useragent_id, .http_method, .path, .useragent]'
