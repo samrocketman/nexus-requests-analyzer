5b358c20ed9db36c9e5d061742bad3f30cd02bb0
Sam Gleske <sam.mxracer@gmail.com>
2026-01-31 08:59:09 -0500

    pass in TSV header

--- replay_requests/requests-to-tsv.sh
+++ replay_requests/requests-to-tsv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-yq -o=tsv '.requests[0].unix_time as $mod | .requests[] | [.unix_time % $mod, .host + "." + .useragent_id, .http_method, .path, .useragent]' input.yaml
+echo $'timestamp_offset\tclient_id\thttp_method\tpath\tuseragent'
+yq -r -o=tsv '.requests[0].unix_time as $mod | .requests[] | [.unix_time % $mod, .host + "." + .useragent_id, .http_method, .path, .useragent]'
